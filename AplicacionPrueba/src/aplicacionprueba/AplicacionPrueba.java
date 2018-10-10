be4e21d56e460c6ba2ec572da211eaf8b1f7270c
richardsis <richard.moisesleon@gmail.com>
2018-10-10 17:25:07 -0500

    Modificando el proyecto inicial 30-09-2018 4:09 pm

--- AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -18,7 +18,8 @@ public class AplicacionPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //modificado por papu
+        // modificado por papu
+        // modificado por richard
         
         // modificando el codigo fuente de mi aplicacion
         System.out.println("Hola Mundo");
